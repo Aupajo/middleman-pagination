86549e582b2dd3cad6d09ed3878186cf934be391
Pete Nicholls <pete@metanation.com>
2013-10-28 15:07:57 +1300

    Refactor ManipulatedResources

--- lib/middleman/pagination/manipulated_resources.rb
+++ lib/middleman/pagination/manipulated_resources.rb
@@ -16,49 +16,49 @@ module Middleman
 
       def new_resources
         context.configuration.map do |name, filter|
-
-          original_resources.map do |resource|
-
-            if resource.data.pagination.try(:for) == name.to_s
-
-              pageable_context = PageableContext.new(
-                per_page: resource.data.pagination.per_page || 20,
-                resources: original_resources.select(&filter),
-                index_resources: [resource]
-              )
-
-              in_page_context = InPageContext.new(
-                pageable_context: pageable_context,
-                page_num: 1
-              )
-
-              resource.add_metadata(:locals => { 'pagination' => in_page_context })
-
-              (2..pageable_context.total_page_num).map do |n|
-
-                sitemap = context.sitemap
-                # TODO use app.index_file
-                path = resource.path.sub(/index\.html$/, "pages/#{n}.html")
-                source_file = resource.source_file
-
-                new_index = ::Middleman::Sitemap::Resource.new(sitemap, path, source_file)
-                
-                in_page_context = InPageContext.new(
-                  pageable_context: pageable_context,
-                  page_num: n
-                )
-
-                new_index.add_metadata(:locals => { 'pagination' => in_page_context })
-                pageable_context.index_resources << new_index
-                new_index
-
-              end
-
-            end
-
-          end.compact
-
+          new_resources_for_pageable(name, filter)
         end.flatten
+      end
+
+      def new_resources_for_pageable(name, filter)
+        original_resources.map do |resource|
+          if resource.data.pagination.try(:for) == name.to_s
+            new_resources_for_index(resource, filter)
+          end
+        end.compact
+      end
+
+      def new_resources_for_index(first_index, filter)
+        pageable_context = PageableContext.new(
+          per_page: first_index.data.pagination.per_page || 20,
+          resources: original_resources.select(&filter),
+          index_resources: [first_index]
+        )
+
+        add_pagination_to(first_index, pageable_context: pageable_context, page_num: 1)
+
+        (2..pageable_context.total_page_num).map do |n|
+          build_new_index(first_index, pageable_context, n)
+        end
+      end
+
+      def build_new_index(first_index, pageable_context, page_num)
+        sitemap = context.sitemap
+        # TODO use app.index_file
+        path = first_index.path.sub(/index\.html$/, "pages/#{page_num}.html")
+        source_file = first_index.source_file
+
+        new_index = ::Middleman::Sitemap::Resource.new(sitemap, path, source_file)
+        add_pagination_to(new_index, pageable_context: pageable_context, page_num: page_num)
+        
+        pageable_context.index_resources << new_index
+
+        new_index
+      end
+
+      def add_pagination_to(resource, attributes = {})
+        in_page_context = InPageContext.new(attributes)
+        resource.add_metadata(:locals => { 'pagination' => in_page_context })
       end
     end
   end
